963f9f3b7a674d40b323b4adb88d8deec718d658
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2020-12-04 11:35:34 -0300

    Initial commit and setup TailwindCSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import '../styles/tailwind.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif']
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
